c8b7690fe5f181a7e28e8a987be29ce65011b019
MarsMinerstone <magic777dance@mail.ru>
2022-05-07 15:44:11 +0700

    Main_Script

---
+++ Assets/LearnMaterials 2/Scripts/Manage_True.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manage_True : MonoBehaviour
+{
+    [SerializeField]
+    private List<SampleScript> script_list;
+
+    // Start is called before the first frame update
+    [ContextMenu("Start")]
+    public void scripts_start()
+    {
+        for (int i = 0; i < script_list.Count; i++)
+        {
+            script_list[i].Use();
+        }
+    }
+
+}
