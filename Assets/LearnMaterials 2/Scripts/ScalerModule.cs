f0075d30ccb5fab43459792b769b0c2567f33438
Behzod2001 <102402712+Behzod2001@users.noreply.github.com>
2022-05-07 14:04:13 +0700

    Change fields Scale Module

--- Assets/LearnMaterials 2/Scripts/ScalerModule.cs
+++ Assets/LearnMaterials 2/Scripts/ScalerModule.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1rdTEVSrCcYOjqTJcFCHj46RvnbdJhmQUb3gHMDhVftI/edit?usp=sharing")]
 public class ScalerModule : MonoBehaviour
 {
+    [SerializeField]
     private Vector3 targetScale = new Vector3(2,2,2);
-
+    [SerializeField]
+    [Min(0)]
     private float changeSpeed;
 
     private Vector3 defaultScale;
@@ -19,6 +21,7 @@ public class ScalerModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Start")]
     public void ActivateModule()
     {
         Vector3 target = toDefault ? defaultScale : targetScale;
